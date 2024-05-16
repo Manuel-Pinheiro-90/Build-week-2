4674d2c3a765c9b9706479fde484121619facb8e
Morgan Grande <morgan.grande@gmail.com>
2024-05-16 17:55:46 +0200

    fixed player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 /* CARDS SEZIONE BUONASERA */
 const albumArray = [
-  "423368",
-  "513551092",
-  "547520122",
-  "119606",
-  "508204251",
-  "11428966",
+    "423368",
+    "513551092",
+    "547520122",
+    "119606",
+    "508204251",
+    "11428966",
 ];
 const generateAlbumCards = function (album) {
-  const row = document.getElementById("album-sera");
+    const row = document.getElementById("album-sera");
 
-  const newCol = document.createElement("div");
-  newCol.classList.add(
-    "col-3",
-    "bg-card-personal",
-    "m-1",
-    "flex-grow-1",
-    "d-flex",
-    "p-0",
-    "size_img"
-  );
+    const newCol = document.createElement("div");
+    newCol.classList.add(
+        "col-3",
+        "bg-card-personal",
+        "m-1",
+        "flex-grow-1",
+        "d-flex",
+        "p-0",
+        "size_img"
+    );
 
-  newCol.innerHTML = `
+    newCol.innerHTML = `
       <div class="container d-flex p-0" onclick="window.location.href=('album.html?albumId=${album.id}')">
           <div class="col-4">
             <img src="${album.cover_medium}" class="h-100" alt="...">
@@ -31,70 +31,72 @@ const generateAlbumCards = function (album) {
           </div>
        </div>  
         `;
-  row.appendChild(newCol);
+    row.appendChild(newCol);
 };
 
 function getAlbumS(albums) {
-  albums.forEach((album) => {
-    getAlbum(album);
-  });
+    albums.forEach((album) => {
+        getAlbum(album);
+    });
 }
 
 const getAlbum = function (id) {
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + id)
-    .then((response) => {
-      if (response.ok) {
-        console.log(response);
-        return response.json();
-      } else {
-        if (response.status === 404) {
-          throw new Error(
-            "Errore nella risposta del server: Resource not found " +
-              response.status
-          );
-        } else if (response.status === 500) {
-          throw new Error(
-            "Errore nella risposta del server: Internal server error " +
-              response.status
-          );
-        } else {
-          throw new Error(
-            "Errore nella risposta del server: Unknown error " + response.status
-          );
-        }
-      }
-    })
-    .then((array) => {
-      console.log("ARRAY!", array);
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + id)
+        .then((response) => {
+            if (response.ok) {
+                console.log(response);
+                return response.json();
+            } else {
+                if (response.status === 404) {
+                    throw new Error(
+                        "Errore nella risposta del server: Resource not found " +
+                        response.status
+                    );
+                } else if (response.status === 500) {
+                    throw new Error(
+                        "Errore nella risposta del server: Internal server error " +
+                        response.status
+                    );
+                } else {
+                    throw new Error(
+                        "Errore nella risposta del server: Unknown error " + response.status
+                    );
+                }
+            }
+        })
+        .then((array) => {
+            console.log("ARRAY!", array);
 
-      generateAlbumCards(array);
-    })
-    .catch((err) => {
-      console.log("ERRORE!", err);
-    });
+            generateAlbumCards(array);
+        })
+        .catch((err) => {
+            console.log("ERRORE!", err);
+        });
 };
 
 /* CARDS SEZIONE ALTRO CHE TI PIACE */
 let carosel_number = 0;
 
 const artistArray = [
-  "2059",
-  "1342",
-  "1125",
-  "900",
-  "226",
-  "1154",
-  "4138",
-  "3968561",
-  "542",
-  "429675",
+    
+    "134790",
+    "226",
+    "1154",
+    "4138",
+    "3968561",
+    "542",
+    "429675",
+    "168632",
+    "409258",
+    "12749837",
+    
 ];
 const generateArtistCards = function (artist) {
-  const row2 = document.getElementById("ads-artist");
+    const row2 = document.getElementById("ads-artist");
 
-  const newCol2 = document.createElement("div");
-  newCol2.classList.add("p-1", "d-flex", "flex-column", "align-items-center");
-  newCol2.innerHTML = `
+    const newCol2 = document.createElement("div");
+    newCol2.classList.add("p-1", "d-flex", "flex-column", "align-items-center");
+    newCol2.innerHTML = `
         <div class="w-100 h-100 card-personal" onclick="window.location.href=('album.html?albumId=${artist.data[0].album.id}')">
         <div class="img-holder">
           <img src="${artist.data[0].album.cover_medium}" class="w-100 h-100 object-fit-cover rounded-2" alt="...">
@@ -112,52 +114,56 @@ const generateArtistCards = function (artist) {
 
 
 const generateCarouselCards = function (artist) {
+    if (carosel_number < 3) {
+        carosel_number++
+        const get_carosel = document.getElementById("carosello_princ")
+        const card_carosel = document.createElement("div")
 
-    carosel_number++
-    const get_carosel = document.getElementById("carosello_princ")
-    const card_carosel = document.createElement("div")
-    if (carosel_number === 1) {
-        card_carosel.classList.add("carousel-item", "active")
-        console.log(carosel_number)
-    }
-    else {
-        card_carosel.classList.add("carousel-item")
-        console.log(carosel_number)
-    }
-    card_carosel.setAttribute("data-bs-interval", '10000')
 
-  card_carosel.innerHTML = `
+        if (carosel_number === 1) {
+            card_carosel.classList.add("carousel-item", "active")
+            console.log(carosel_number)
+        }
+        else {
+            card_carosel.classList.add("carousel-item")
+            console.log(carosel_number)
+        }
+        card_carosel.setAttribute("data-bs-interval", '10000')
+
+        card_carosel.innerHTML = `
     <div class="card mb-3 bg-dark bg-gradient">
-                <div class="row g-0">
+    <div class="row g-0">
                   <div class="col-md-4">
                     <img src="${artist.data[0].album.cover_medium}" class="img-fluid rounded-start"
                       alt="imgCopertina" />
-                  </div>
-                  <div class="col-md-8 d-flex">
-                    <div class="card-body d-flex flex-column">
+                      </div>
+                      <div class="col-md-8 d-flex">
+                      <div class="card-body d-flex flex-column">
                       <h1 class="card-title text-light fs-title">${artist.data[0].title_short}</h1>
                       <p class="card-text text-light">
-                        ${artist.data[0].artist.name}
+                      ${artist.data[0].artist.name}
                       </p>
                       <p class="card-text">
-                        <small class="text-light">Ascolta ${artist.data[0].title_short} dall'album: ${artist.data[0].album.title}</small>
+                      <small class="text-light">Ascolta ${artist.data[0].title_short} dall'album: ${artist.data[0].album.title}</small>
                       </p>
                       <div>
-                        <button class='btn btn-success rounded-5 play${carosel_number}'>Play</button>
-                        <button class="btn btn-dark rounded-5">Salva</button>
+                      <button class='btn btn-success rounded-5 play${carosel_number}'>Play</button>
+                      <button class="btn btn-dark rounded-5">Salva</button>
                       </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-    `
-    // onclick = "autoStart(${artist.data[0].album.title},${artist.data[0].artist.name},${artist.data[0].album.cover_small},${artist.data[0].preview})"
-    get_carosel.appendChild(card_carosel)
+                      </div>
+                      </div>
+                      </div>
+                      </div>
+                      `
+        // onclick = "autoStart(${artist.data[0].album.title},${artist.data[0].artist.name},${artist.data[0].album.cover_small},${artist.data[0].preview})"
+        get_carosel.appendChild(card_carosel)
 
 
-    addplayerlisten(artist, carosel_number)
+        addplayerlisten(artist, carosel_number)
 
 
+
+    }
 }
 
 
@@ -219,15 +225,15 @@ function autoStart(array) {
         audioElement.pause(); // Pause the current audio
         audioElement.remove(); // Remove the audio element from the DOM
     }
-    
+
     audioElement = new Audio(array.data[0].preview);
     titol.innerHTML = array.data[0].title
     artis.innerHTML = array.data[0].artist.name
     img.src = array.data[0].album.cover_small
     playPauseButton.click();
-    
+    document.getElementById('musician').classList.remove('d-none');
 
-    audioElement.ontimeupdate = function() {timeupdate()};
+    audioElement.ontimeupdate = function () { timeupdate() };
 }
 
 let audioElement = new Audio('');
@@ -243,15 +249,15 @@ const volumeProgress = document.querySelector('#volume-progress');
 
 // Avvia o metti in pausa la riproduzione audio al clic del pulsante Play/Pausa
 playPauseButton.addEventListener("click", () => {
-  if (audioElement.paused) {
-    audioElement.play();
-    playPauseButton.classList.remove("fa-play");
-    playPauseButton.classList.add("fa-pause");
-  } else {
-    audioElement.pause();
-    playPauseButton.classList.remove("fa-pause");
-    playPauseButton.classList.add("fa-play");
-  }
+    if (audioElement.paused) {
+        audioElement.play();
+        playPauseButton.classList.remove("fa-play");
+        playPauseButton.classList.add("fa-pause");
+    } else {
+        audioElement.pause();
+        playPauseButton.classList.remove("fa-pause");
+        playPauseButton.classList.add("fa-play");
+    }
 });
 
 // Aggiorna la posizione della canzone quando l'utente interagisce con la barra di avanzamento
@@ -281,7 +287,7 @@ volumeBar.addEventListener('click', (e) => {
     console.log('Volume:', volume);
     audioElement.volume = volume;
     console.log('Audio volume set to:', volume);
-    
+
     const volumeWi = audioElement.volume * 100;
     volumeProgress.style.width = `${volumeWi}%`;
 });
@@ -289,7 +295,7 @@ volumeBar.addEventListener('click', (e) => {
 // Aggiorna la barra di avanzamento della canzone e del volume
 // audioElement.ontimeupdate = function() {timeupdate()};
 
-function timeupdate(){
+function timeupdate() {
     const currentTime = formatTime(audioElement.currentTime);
     const duration = formatTime(audioElement.duration);
     const progressWidth = (audioElement.currentTime / audioElement.duration) * 100;
@@ -304,8 +310,8 @@ function timeupdate(){
 }
 // Funzione per formattare il tempo in formato mm:ss
 function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
